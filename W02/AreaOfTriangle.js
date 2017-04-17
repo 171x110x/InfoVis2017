dec2de2d8800ae5acbf5cfb9eeba9a48b142a04e
Tsuyoshi <tsuyoshi@kitamura-mba.local>
2017-04-18 08:01:31 +0900

    Initial commit

--- W02/AreaOfTriangle.js
+++ W02/AreaOfTriangle.js
@@ -2,7 +2,7 @@ function AreaOfTriangle(v0, v1, v2)
 {   
     // vec 01
 	var xa = v1.x - v0.x,
-	    ya = v1.y - v0.y,
+		ya = v1.y - v0.y,
 		za = v1.z - v0.z;
 
 	// vec 12
@@ -13,7 +13,7 @@ function AreaOfTriangle(v0, v1, v2)
 	// calc corss product
 	var xg = (ya * zb) - (za * yb);
 		yg = (za * xb) - (xa * zb),
-	 	zg = (xa * yb) - (ya * xb);
+		zg = (xa * yb) - (ya * xb);
 
 	// calc area
 	var tmp = Math.pow(xg, 2) + Math.pow(yg, 2) + Math.pow(zg, 2);
